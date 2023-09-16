657df33da1004d06e3c9d6ceb75b25a1b60f6978
Andreas <andy.reitberger@gmail.com>
2023-09-16 11:29:32 +0200

    Added example project

---
+++ src/MauiAppNet8Test/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppNet8Test
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ src/MauiAppNet8Test/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiAppNet8Test
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
